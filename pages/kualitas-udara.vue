e070b2afdfe15ddb5ad905c626490c644bbc8a63
Rahmat-Afriyadi <rahmatcv725@gmail.com>
2025-04-06 20:06:54 +0700

    nambahin dummy page

---
+++ pages/kualitas-udara.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-full h-screen flex justify-center items-center">
+    <p class="text-2xl font-bold">Kualitas Udara</p>
+  </div>
+</template>
+
+<script setup>
+// Tidak perlu JS khusus untuk tampilan statis ini
+</script>
+
+<style>
+/* Styling tambahan jika diperlukan */
+</style>
